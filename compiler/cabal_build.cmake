4bd6d042b81c2d0b745cdaaf3a486c8a22d62fbc
Adam Sapek <adamsap@microsoft.com>
2015-02-04 20:00:45 -0800

    Update appveyor.yml for the open source plan
    Made the following changes for the open source AppVeyor plan which
    runs on very slow and memory limited VMs:
    - Limit ghc heap size to 192MB when installing dependencies
    - Use `appveyor DownloadFile` instead `Start-Download`
    - Download Boost in the smaller 7Zip format
    - Cache cabal sandbox and boost download
    - Disable C++ build

--- compiler/cabal_build.cmake
+++ compiler/cabal_build.cmake
@@ -6,8 +6,16 @@
 # default cabal install uses directory under %APPDATA% which contains user name.
 # As a workaround we execute this .cmake script as a custom command and use CMake
 # cache to get access to variables set during configuration.
+
+cmake_policy (SET CMP0012 NEW)
+
+if ($ENV{APPVEYOR})
+    # AppVeyor Azure VMs have limited memory, limit ghc heap to 192 MB
+    set (GHC_OPTIONS --ghc-option=+RTS --ghc-option=-M192m --ghc-option=-RTS)
+endif()
+
 execute_process (
-    COMMAND ${Haskell_CABAL_EXECUTABLE} install   --with-compiler=${Haskell_GHC_EXECUTABLE} --only-dependencies --jobs
+    COMMAND ${Haskell_CABAL_EXECUTABLE} install   --with-compiler=${Haskell_GHC_EXECUTABLE} --only-dependencies --jobs ${GHC_OPTIONS}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     RESULT_VARIABLE error)
 
@@ -23,7 +31,7 @@ execute_process (
 if (error)
     message (FATAL_ERROR)
 endif()
-    
+
 execute_process (
     COMMAND ${Haskell_CABAL_EXECUTABLE} build     --with-ghc=${Haskell_GHC_EXECUTABLE} --ghc-option=-O2 --jobs --builddir=${output_dir}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
@@ -32,4 +40,4 @@ execute_process (
 if (error)
     message (FATAL_ERROR)
 endif()
-    
+
