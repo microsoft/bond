e4dd2a8cd1fdb8c3f61fbaa50988b7c005491fae
Christopher Warrington <chwarr@microsoft.com>
2016-02-25 23:33:43 -0800

    Add skeleton of Comm interfaces project files

---
+++ cs/src/comm/interfaces/interfaces.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Bond.Comm.Interfaces
+{
+    public static class SimpleAdder
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ cs/test/comm/InterfacesTest.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTest
+{
+    using Bond.Comm.Interfaces;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AttributesTests
+    {
+        [Test]
+        public void SimpleAdder_DoesAddition()
+        {
+            Assert.AreEqual(12, SimpleAdder.Add(5, 7));
+        }
+    }
+}
