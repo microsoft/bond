0d288daff78b5068e91ec23536683787503a0c0f
Christopher Warrington <chwarr@microsoft.com>
2016-03-03 08:41:34 -0800

    Add enum limits to enumeration examples
    * Add new enum with int32/uint32 limits to existing C++ example
    * Add entierly new C# enumeration example

--- examples/cpp/core/enumerations/enumerations.cpp
+++ examples/cpp/core/enumerations/enumerations.cpp
@@ -1,3 +1,7 @@
+#include <cassert>
+#include <limits>
+#include <stdint.h>
+
 #include "enumerations_enum.h"
 
 using namespace examples::enumerations;
@@ -5,14 +9,19 @@ using namespace examples::enumerations;
 int main()
 {
     Color color;
-    
+
     color = Color::Orange;
     color = Yellow;
 
     Fruit fruit;
-    
-    fruit = Apple;
-    fruit = Fruit::Orange;    
 
-    return 0;    
+    fruit = Apple;
+    fruit = Fruit::Orange;
+
+    assert(Limits::Int32Min == std::numeric_limits<int32_t>::min());
+    assert(Limits::Int32Max == std::numeric_limits<int32_t>::max());
+    assert(Limits::UInt32Min == std::numeric_limits<uint32_t>::min());
+    assert(Limits::UInt32Max == std::numeric_limits<uint32_t>::max());
+
+    return 0;
 }
---
+++ examples/cs/core/enumerations/program.cs
@@ -0,0 +1,22 @@
+﻿namespace Examples
+{
+    using System;
+    using System.Diagnostics;
+
+    static class Program
+    {
+        static void Main()
+        {
+            var yellow = Color.Yellow;
+            Debug.Assert((int)yellow == 2);
+
+            var apple = Fruit.Apple;
+            Debug.Assert((int)apple == 1);
+
+            Debug.Assert((Int32)Limits.Int32Min == Int32.MinValue);
+            Debug.Assert((Int32)Limits.Int32Max == Int32.MaxValue);
+            Debug.Assert((UInt32)Limits.UInt32Min == UInt32.MinValue);
+            Debug.Assert(unchecked((UInt32)Limits.UInt32Max) == UInt32.MaxValue);
+        }
+    }
+}
