c9dc5bc5c1955f61b404cab2c1f32f91a923c162
Christopher Warrington <chwarr@microsoft.com>
2016-03-18 14:38:17 -0700

    Adjust Message type to support errors and payloads
    * Message can now be constructed with derived payloads without
      runtime errors during serialization/deserialization.
    * Message.FromError must be used to create Messages with error payloads
      to avoid overload ambiguity in some cases.
    * Unit tests added for Message class.

--- cs/src/comm/interfaces/Message.cs
+++ cs/src/comm/interfaces/Message.cs
@@ -5,10 +5,46 @@ namespace Bond.Comm
 {
     using System;
     using System.Diagnostics;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class Message
+    {
+        public static Message<TPayload> FromPayload<TPayload>(TPayload payload)
+        {
+            return FromPayload(MakeIBonded(payload));
+        }
+
+        public static Message<TPayload> FromPayload<TPayload>(IBonded<TPayload> payload)
+        {
+            return new Message<TPayload>(payload);
+        }
+
+        public static Message<TPayload> FromError<TPayload>(Error err)
+        {
+            return FromError<TPayload>(MakeIBonded(err));
+        }
+
+        public static Message<TPayload> FromError<TPayload>(IBonded<Error> err)
+        {
+            return new Message<TPayload>(err);
+        }
+
+        internal static IBonded<TBonded> MakeIBonded<TBonded>(TBonded payload)
+        {
+            var payloadBondedType = typeof (Bonded<>).MakeGenericType(payload.GetType());
+            var ctor = payloadBondedType.GetTypeInfo().DeclaredConstructors.First();
+            var bonded = ctor.Invoke(new object[] { payload });
+            return (IBonded<TBonded>)bonded;
+        }
+    }
 
     public class Message<TPayload>
     {
-        public Message(TPayload payload) : this(new Bonded<TPayload>(payload)) { }
+        private IBonded<TPayload> m_payload;
+        private IBonded<Error> m_error;
+
+        public Message(TPayload payload) : this(Message.MakeIBonded(payload)) { }
 
         public Message(IBonded<TPayload> payload)
         {
@@ -16,21 +52,18 @@ namespace Bond.Comm
             m_error = null;
         }
 
-        public Message(Error error) : this(new Bonded<Error>(error)) { }
-
-        public Message(IBonded<Error> error)
+        // To create an error Message, use Message<TPayload>.FromError().
+        //
+        // This ctor is internal so that a non-error Message<Error> can be created. If this
+        // were public, then new Message<Error>(SomeError) would resolve to this ctor, creating an
+        // Error message, instead of to the generic ctor. We need new Message<Error>(SomeError) to
+        // resolve to the generic ctor to create a non-error Message.
+        internal Message(IBonded<Error> error)
         {
             m_payload = null;
             m_error = error;
         }
 
-        private IBonded<TPayload> m_payload;
-        // Could use bool and have one IBonded field for both. Let's let perf tests show the way.
-        private IBonded<Error> m_error;
-
-        // Should there be some way to replace the payload with a live
-        // object if, say, a layer need to fully deserializes the
-        // payload?
         public IBonded<TPayload> Payload
         {
             get
---
+++ cs/test/comm/Tcp/MessageTests.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace UnitTest
+{
+    using System;
+
+    using Bond;
+    using Bond.Comm;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class MessageTests
+    {
+        private static readonly Error AnyError = new Error
+        {
+            error_code = 1,
+        };
+
+        private static readonly SomePayloadType AnyPayload = new SomePayloadType
+        {
+            int_field = 100,
+        };
+
+        [Test]
+        public void Construct_WithPayload_IsPayload()
+        {
+            var msg = new Message<SomePayloadType>(AnyPayload);
+
+            Assert.IsFalse(msg.IsError);
+            Assert.IsNull(msg.Error);
+
+            var deserializedPayload = msg.Payload.Deserialize();
+            Assert.IsTrue(AnyPayload.IsEqual<SomePayloadType>(deserializedPayload));
+        }
+
+        [Test]
+        public void FromPayload_WithPayload_IsPayload()
+        {
+            var msg = Message.FromPayload(AnyPayload);
+
+            Assert.IsFalse(msg.IsError);
+            Assert.IsNull(msg.Error);
+
+            var deserializedPayload = msg.Payload.Deserialize();
+            Assert.IsTrue(AnyPayload.IsEqual<SomePayloadType>(deserializedPayload));
+        }
+
+        [Test]
+        public void Construct_WithBonded_IsPayload()
+        {
+            var bondedPayload = new Bonded<SomePayloadType>(AnyPayload);
+            var msg = new Message<SomePayloadType>(bondedPayload);
+            Assert.IsFalse(msg.IsError);
+            Assert.IsNull(msg.Error);
+
+            var deserializedPayload = msg.Payload.Deserialize();
+            Assert.IsTrue(AnyPayload.IsEqual<SomePayloadType>(deserializedPayload));
+        }
+
+        [Test]
+        public void Construct_WithDerivedPayload_IsPayload()
+        {
+            var derivedPayload = new SomeDerivedPayloadType
+            {
+                int_field = 100,
+                bool_field = true,
+            };
+
+            var msg = new Message<SomePayloadType>(derivedPayload);
+            Assert.IsFalse(msg.IsError);
+            Assert.IsNull(msg.Error);
+
+            var deserializedPayload = msg.Payload.Deserialize<SomeDerivedPayloadType>();
+            Assert.IsTrue(derivedPayload.IsEqual<SomeDerivedPayloadType>(deserializedPayload));
+        }
+
+        [Test]
+        public void FromPayload_WithDerivedPayload_IsPayload()
+        {
+            var derivedPayload = new SomeDerivedPayloadType
+            {
+                int_field = 100,
+                bool_field = true,
+            };
+
+            var msg = Message.FromPayload<SomePayloadType>(derivedPayload);
+            Assert.IsFalse(msg.IsError);
+            Assert.IsNull(msg.Error);
+
+            var deserializedPayload = msg.Payload.Deserialize<SomeDerivedPayloadType>();
+            Assert.IsTrue(derivedPayload.IsEqual<SomeDerivedPayloadType>(deserializedPayload));
+        }
+
+        [Test]
+        public void Construct_WithError_IsPayload()
+        {
+            var msg = new Message<Error>(AnyError);
+
+            Assert.IsFalse(msg.IsError);
+            Assert.IsNull(msg.Error);
+
+            var deserializedPayload = msg.Payload.Deserialize();
+            Assert.IsTrue(AnyError.IsEqual<Error>(deserializedPayload));
+        }
+
+        [Test]
+        public void FromError_WithError_IsError()
+        {
+            var msg = Message.FromError<Error>(AnyError);
+
+            Assert.IsTrue(msg.IsError);
+            Assert.IsNotNull(msg.Error);
+            Assert.IsTrue(AnyError.IsEqual<Error>(msg.Error.Deserialize()));
+
+            Assert.Throws<InvalidOperationException>(() => { var _ = msg.Payload; });
+        }
+
+        [Test]
+        public void FromError_WithBondedError_IsError()
+        {
+            var msg = Message.FromError<Error>(new Bonded<Error>(AnyError));
+
+            Assert.IsTrue(msg.IsError);
+            Assert.IsNotNull(msg.Error);
+            Assert.IsTrue(AnyError.IsEqual<Error>(msg.Error.Deserialize()));
+
+            Assert.Throws<InvalidOperationException>(() => { var _ = msg.Payload; });
+        }
+
+        [Schema]
+        private class SomePayloadType
+        {
+            [Bond.Id(0)] public int int_field;
+        }
+
+        [Schema]
+        private class SomeDerivedPayloadType : SomePayloadType
+        {
+            [Bond.Id(0)] public bool bool_field;
+        }
+    }
+}
