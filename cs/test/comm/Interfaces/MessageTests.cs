7922e91fd2bdebde6df56df75a099645cfd6ca46
Christopher Warrington <chwarr@microsoft.com>
2016-04-11 17:25:38 -0700

    Move Message tests to Interfaces
    Message is part of the interfaces, but was accidentally put with the TCP
    tests.

--- cs/test/comm/Interfaces/MessageTests.cs
+++ cs/test/comm/Interfaces/MessageTests.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace UnitTest.Tcp
+namespace UnitTest.Interfaces
 {
     using System;
     using Bond;
