5bbe73c7b19f1969483d4743b4fa4b6ab51e8451
Christopher Warrington <chwarr@microsoft.com>
2016-04-26 15:05:15 -0700

    Test Compact Binary v2 compat between C# and C++

--- cpp/test/compat/compat.cmake
+++ cpp/test/compat/compat.cmake
@@ -13,18 +13,10 @@ if (${TEST} STREQUAL schema)
 endif()
 
 if (CSHARP_COMPAT)
-    set (TEST_CS ${TEST})
-
-    # C# implements reading of Compact Binary v2 but not writing
-    if (${TEST_CS} STREQUAL compact2)
-        set (TEST_CS compact)
-    endif()
-
-    run (${CSHARP_COMPAT} ${TEST} ${COMPAT_DATA}/compat.${TEST}.dat compat.${TEST}.cs.dat ${TEST_CS})
-    run (${BOND_COMPAT} ${TEST_CS} -d compat.${TEST}.cs.dat expected.cs.${TEST} deserialized.cs.${TEST})
+    run (${CSHARP_COMPAT} ${TEST} ${COMPAT_DATA}/compat.${TEST}.dat compat.${TEST}.cs.dat ${TEST})
+    run (${BOND_COMPAT} ${TEST} -d compat.${TEST}.cs.dat expected.cs.${TEST} deserialized.cs.${TEST})
 
     if (${TEST} STREQUAL schema)
         run (${CSHARP_COMPAT} ${TEST} compat.Compat.json compat.${TEST}.gbc.dat)
     endif()
 endif()
-
