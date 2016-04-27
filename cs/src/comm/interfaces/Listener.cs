f82e0cc7a49fef0cdba9beb6d04e2f1d324e358a
Christopher Warrington <chwarr@microsoft.com>
2016-04-26 18:30:04 -0700

    Add Listener.Connected and .Disconnected events
    * Listener.OnConnected and OnDisconnected virtual methods added to
      trigger the Connected and Disconnected events.
    * Unit tests for these methods
    * Minor refactoring of test classes.

--- cs/src/comm/interfaces/Listener.cs
+++ cs/src/comm/interfaces/Listener.cs
@@ -83,16 +83,32 @@ namespace Bond.Comm
         /// Occurs when a new connection has being established.
         /// </summary>
         /// <remarks>
+        /// <para>
         /// The Connected event is raised when a new connection has been
         /// established but before any messages have been transmitted across
         /// it. The event can be used to setup connection-specific data
         /// structures or to reject the connection.
+        /// </para>
+        /// <para>
+        /// Connected event handlers must not throw exceptions. The Listener
+        /// has no sensible way to handle the exception, and there is no way to
+        /// bubble the exception back to the application code. If an exception
+        /// is thrown, the process will be terminated.
+        /// </para>
         /// </remarks>
         public event EventHandler<ConnectedEventArgs> Connected;
 
         /// <summary>
         /// Occurs when a connection has been disconnected.
         /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Disconnected event handlers must not throw exceptions. The Listener
+        /// has no sensible way to handle the exception, and there is no way to
+        /// bubble the exception back to the application code. If an exception
+        /// is thrown, the process will be terminated.
+        /// </para>
+        /// </remarks>
         public event EventHandler<DisconnectedEventArgs> Disconnected;
 
         /// <summary>
@@ -146,5 +162,65 @@ namespace Bond.Comm
         /// A task that represents the asynchronous operation.
         /// </returns>
         public abstract Task StopAsync();
+
+        protected virtual Error OnConnected(ConnectedEventArgs args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            // make a local copy so we can iterate through it even if it gets
+            // changed while we're iterating
+            EventHandler<ConnectedEventArgs> connectedEventHandler = Connected;
+
+            if (connectedEventHandler != null)
+            {
+                // invoke one at a time so that we can check whether
+                // DisconnectError was set to non-null and halt processing the
+                // moment it is set to null
+                foreach (var handler in connectedEventHandler.GetInvocationList())
+                {
+                    var connectedHandler = (EventHandler<ConnectedEventArgs>) handler;
+
+                    try
+                    {
+                        connectedHandler.Invoke(this, args);
+                    }
+                    catch (Exception ex)
+                    {
+                        Transport.FailFastExceptionHandler(ex);
+                    }
+
+                    if (args.DisconnectError != null)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return args.DisconnectError;
+        }
+
+        protected virtual void OnDisconnected(DisconnectedEventArgs args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            // make a local copy so we can invoke it even if it gets changed
+            // while we're invoking
+            EventHandler<DisconnectedEventArgs> disconnectedEventHandler = Disconnected;
+
+            try
+            {
+                disconnectedEventHandler?.Invoke(this, args);
+            }
+            catch (Exception ex)
+            {
+                Transport.FailFastExceptionHandler(ex);
+            }
+        }
     }
 }
---
+++ cs/test/comm/Interfaces/ListenerTests.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace UnitTest.Interfaces
+{
+    using System;
+    using System.Threading.Tasks;
+    using Bond.Comm;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ListenerTests
+    {
+        [Test]
+        public void Connected_NoSubscribedEvents_Works()
+        {
+            new TestListener().Test_OnConnected(new ConnectedEventArgs(null));
+        }
+
+        [Test]
+        public void Connected_MultipleSubscribedEvents_CalledInOrderUntilOneSetsDisconnectErrorToNonNull()
+        {
+            bool event1Called = false;
+            bool event2Called = false;
+            var event2Error = new Error();
+            bool event3Called = false;
+            var event3Error = new Error();
+
+            var listener = new TestListener();
+            listener.Connected += (sender, args) =>
+            {
+                Assert.IsFalse(event1Called);
+                Assert.IsFalse(event2Called);
+                Assert.IsFalse(event3Called);
+                event1Called = true;
+            };
+            listener.Connected += (sender, args) =>
+            {
+                Assert.IsTrue(event1Called);
+                Assert.IsFalse(event2Called);
+                Assert.IsFalse(event3Called);
+                event2Called = true;
+                args.DisconnectError = event2Error;
+            };
+            listener.Connected += (sender, args) =>
+            {
+                Assert.IsTrue(event1Called);
+                Assert.IsTrue(event2Called);
+                Assert.IsFalse(event3Called);
+                event3Called = true;
+                args.DisconnectError = event3Error;
+            };
+
+            var cea = new ConnectedEventArgs(null);
+            listener.Test_OnConnected(cea);
+
+            Assert.IsTrue(event1Called);
+            Assert.IsTrue(event2Called);
+            Assert.IsFalse(event3Called);
+            Assert.AreSame(event2Error, cea.DisconnectError);
+        }
+
+        [Test]
+        public void Disconnected_NoSubscribedEvents_Works()
+        {
+            new TestListener().Test_OnDisconnected(new DisconnectedEventArgs(null, null));
+        }
+
+        [Test]
+        public void Disconnected_MultipleSubscribedEvents_AllCalledAndInOrder()
+        {
+            bool event1Called = false;
+            bool event2Called = false;
+
+            var listener = new TestListener();
+            listener.Disconnected += (sender, eventArgs) =>
+            {
+                Assert.IsFalse(event1Called);
+                Assert.IsFalse(event2Called);
+                event1Called = true;
+            };
+            listener.Disconnected += (sender, eventArgs) =>
+            {
+                Assert.IsTrue(event1Called);
+                Assert.IsFalse(event2Called);
+                event2Called = true;
+            };
+
+            listener.Test_OnDisconnected(new DisconnectedEventArgs(null, null));
+
+            Assert.IsTrue(event1Called);
+            Assert.IsTrue(event2Called);
+        }
+
+        private class TestListener : Listener
+        {
+            public override void AddService<T>(T service) { throw new NotImplementedException(); }
+            public override void RemoveService<T>(T service) { throw new NotImplementedException(); }
+            public override bool IsRegistered(string serviceMethodName) { throw new NotImplementedException(); }
+            public override Task StartAsync() { throw new NotImplementedException(); }
+            public override Task StopAsync() { throw new NotImplementedException(); }
+
+            public Error Test_OnConnected(ConnectedEventArgs args)
+            {
+                return OnConnected(args);
+            }
+
+            public void Test_OnDisconnected(DisconnectedEventArgs args)
+            {
+                OnDisconnected(args);
+            }
+        }
+    }
+}
