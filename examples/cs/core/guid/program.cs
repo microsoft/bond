c1a0aaf695701e37d9b461455c825ca93a56d454
Ming Han Teh <miteh@microsoft.com>
2016-06-19 16:11:14 -0700

    Add C# example to demonstrate storing Guid as a binary blob.

--- examples/cs/core/guid/program.cs
+++ examples/cs/core/guid/program.cs
@@ -8,6 +8,8 @@
 
     public static class BondTypeAliasConverter
     {
+        #region Guid-string conversion
+
         public static Guid Convert(string value, Guid unused)
         {
             return new Guid(value);
@@ -17,6 +19,24 @@
         {
             return value.ToString();
         }
+
+        #endregion
+
+        #region Guid-blob conversion
+
+        public static Guid Convert(ArraySegment<byte> value, Guid unused)
+        {
+            var bits = new byte[16];
+            Buffer.BlockCopy(value.Array, value.Offset, bits, 0, 16);
+            return new Guid(bits);
+        }
+
+        public static ArraySegment<byte> Convert(Guid value, ArraySegment<byte> unused)
+        {
+            return new ArraySegment<byte>(value.ToByteArray(), 0, 16);
+        }
+
+        #endregion
     }
 
     static class Program
@@ -25,7 +45,8 @@
         {
             var src = new Example
             {
-                id = new Guid("{DC37ECC5-9E39-49C9-931B-51138D648262}")
+                id = new Guid("{DC37ECC5-9E39-49C9-931B-51138D648262}"),
+                id_bin = new Guid("{0F5F6768-1608-4D5C-A11D-B176D0D792B5}"),
             };
 
             var output = new OutputBuffer();
@@ -38,6 +59,7 @@
 
             var dst = Deserialize<Example>.From(reader);
             Debug.Assert(dst.id == src.id);
+            Debug.Assert(dst.id_bin == src.id_bin);
         }
     }
 }
