eee2cebde714c838ddb42eddfa7b4a99496582e1
Christopher Warrington <chwarr@microsoft.com>
2016-10-12 18:44:29 -0700

    Bring C++ and C# serialization examples in sync
    Both examples now use the same schema and populate the object with the same
    values.

--- examples/cs/core/serialization/program.cs
+++ examples/cs/core/serialization/program.cs
@@ -1,33 +1,48 @@
 ﻿namespace Examples
 {
+    using System.Collections.Generic;
     using System.Diagnostics;
     using Bond;
     using Bond.Protocols;
     using Bond.IO.Unsafe;
 
+    using examples.serialization;
+
     static class Program
     {
         static void Main()
         {
-            var src = new Example
+            var obj = new Struct
             {
-                Name = "FooBar",
-                Constants = {  3.14, 6.28 }
+                n = 0x1000,
+                str = "test",
+                items = new List<double> { 3.14, 0}
             };
 
+            // OutputBuffer implements the Bond output stream interface on top
+            // of a memory buffer.
             var output = new OutputBuffer();
+            // Use the Compact Binary protocol for encoding; the data will be
+            // written to the OutputBuffer which will allocate memory as needed.
             var writer = new CompactBinaryWriter<OutputBuffer>(output);
 
             // The first calls to Serialize.To and Deserialize<T>.From can take
-            // a relatively long time because they generate the de/serializer 
-            // for a given type and protocol.
-            Serialize.To(writer, src);
+            // a relatively long time because they generate the
+            // serializer/deserializer for a given type, protocol, and stream.
+            // See the serializer example for a way to control this.
+            Serialize.To(writer, obj);
 
+            // At this point the OutputBuffer contains a serialized
+            // representation of the object.
+
+            // Use the Compact Binary protocol for decoding (the same protocol
+            // that was used for encoding) and InputBuffer which implements the
+            // input stream interface on top of a memory blob.
             var input = new InputBuffer(output.Data);
             var reader = new CompactBinaryReader<InputBuffer>(input);
 
-            var dst = Deserialize<Example>.From(reader);
-            Debug.Assert(Comparer.Equal(src, dst));
+            var obj2 = Deserialize<Struct>.From(reader);
+            Debug.Assert(Comparer.Equal(obj, obj2));
         }
     }
 }
