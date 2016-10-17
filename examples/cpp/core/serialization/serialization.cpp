857e87fd2baf6293d94cec6deb0c2c50b116097f
Christopher Warrington <chwarr@microsoft.com>
2016-10-17 15:19:27 -0700

    Bring C++ and C# serialization examples in sync
    Both examples now use the same schema and populate the object with the same
    values.
    
    Closes https://github.com/Microsoft/bond/pull/236

--- examples/cpp/core/serialization/serialization.cpp
+++ examples/cpp/core/serialization/serialization.cpp
@@ -8,38 +8,39 @@ using namespace examples::serialization;
 
 int main()
 {
-    Struct obj, obj2;
+    Struct obj;
 
     obj.n = 0x1000;
     obj.str = "test";
     obj.items.push_back(3.14);
     obj.items.push_back(0);
 
-    // bond::OutputBuffer implements Bond output stream interface on top of
+    // bond::OutputBuffer implements the Bond output stream interface on top of
     // a series of memory buffers.
     bond::OutputBuffer output;
-    
-    // Use Compact Binary protocol for encoding; the data will be written to
+
+    // Use the Compact Binary protocol for encoding; the data will be written to
     // the OutputBuffer which will allocate memory as needed.
     bond::CompactBinaryWriter<bond::OutputBuffer> writer(output);
 
     // Serialize the object
     Serialize(obj, writer);
 
-    // At this point the OutputBuffer contains serialized representation of the
-    // object.  Depending on the size, serialized data may be spread across one
-    // or more memory blocks. Application can get all these blocks using
-    // GetBuffers method, or merge them into a single block using GetBuffer
-    // method.
+    // At this point the OutputBuffer contains a serialized representation of
+    // the object. Depending on the size, the serialized data may be spread
+    // across one or more memory blocks. Applications can get all these blocks
+    // using the GetBuffers method, or merge them into a single block using
+    // GetBuffer method.
     bond::blob data = output.GetBuffer();
 
-    // Use Compact Binary protocol for decoding (the same protocol that was used
-    // for encoding) and InputBuffer which implements input stream interface on
-    // top of a memory blob.
+    // Use the Compact Binary protocol for decoding (the same protocol that was
+    // used for encoding) and InputBuffer which implements the input stream
+    // interface on top of a memory blob.
     bond::CompactBinaryReader<bond::InputBuffer> reader(data);
-    
+
     // De-serialize the object
+    Struct obj2;
     Deserialize(reader, obj2);
 
-    return 0;    
+    return 0;
 }
