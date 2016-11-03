30b2c660a649fd6fc9d49e5df9d4bca0b33062cb
Christopher Warrington <chwarr@microsoft.com>
2016-11-02 20:39:42 -0700

    [c#] Swap a custom list for SynchronizedCollection
    SynchronizedCollection is not present in .NET Standard 1.6, among
    others. It was only being used to demonstrate how collection type
    aliasing works, so it has been replaced with SomeCustomList<T> which
    just wraps List<T> to demonstrate how this would work.
    
    Closes https://github.com/Microsoft/bond/pull/250

--- cs/test/core/TypeAliasTests.cs
+++ cs/test/core/TypeAliasTests.cs
@@ -1,10 +1,11 @@
 ﻿namespace UnitTest.Aliases
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
     using Bond;
     using NUnit.Framework;
-    using System.Linq;
 
     public class Lazy<T> : IBonded<T>
     {
@@ -69,7 +70,7 @@
 
         public override bool Equals(object obj)
         {
-            return Comparer.Equal(Value, (obj as Lazy<T>).Value);
+            return Bond.Comparer.Equal(Value, (obj as Lazy<T>).Value);
         }
 
         public override int GetHashCode()
@@ -280,6 +281,57 @@
 
             var to = Clone<T>.From(Clone<U>.From(from));
             Assert.IsTrue(from.IsEqual<T>(to));
+        }
+    }
+
+    // An extremely simple example of a custom container implementation.
+    public class SomeCustomList<T> : ICollection<T>
+    {
+        readonly List<T> backingList = new List<T>();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return backingList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)backingList).GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            backingList.Add(item);
+        }
+
+        public void Clear()
+        {
+            backingList.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return backingList.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            backingList.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            return backingList.Remove(item);
+        }
+
+        public int Count
+        {
+            get { return backingList.Count; }
+        }
+
+        bool ICollection<T>.IsReadOnly
+        {
+            get { return ((ICollection<T>)backingList).IsReadOnly; }
         }
     }
 }
--- examples/cs/core/container_alias/program.cs
+++ examples/cs/core/container_alias/program.cs
@@ -1,9 +1,11 @@
 ﻿namespace Examples
 {
+    using System.Collections;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using Bond;
-    using Bond.Protocols;
     using Bond.IO.Unsafe;
+    using Bond.Protocols;
 
     static class Program
     {
@@ -24,7 +26,58 @@
             var reader = new CompactBinaryReader<InputBuffer>(input);
 
             var dst = Deserialize<Example>.From(reader);
-            Debug.Assert(Comparer.Equal(src, dst));
+            Debug.Assert(Bond.Comparer.Equal(src, dst));
+        }
+    }
+
+    // An extremely simple example of a custom container implementation.
+    public class SomeCustomList<T> : ICollection<T>
+    {
+        readonly List<T> backingList = new List<T>();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return backingList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)backingList).GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            backingList.Add(item);
+        }
+
+        public void Clear()
+        {
+            backingList.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return backingList.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            backingList.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            return backingList.Remove(item);
+        }
+
+        public int Count
+        {
+            get { return backingList.Count; }
+        }
+
+        bool ICollection<T>.IsReadOnly
+        {
+            get { return ((ICollection<T>) backingList).IsReadOnly; }
         }
     }
 }
