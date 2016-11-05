36def65c8c0cbd60d0af234b436ec072df8b13c9
Christopher Warrington <chwarr@microsoft.com>
2016-11-04 18:08:53 -0700

    [c#] Target .NET Standard 1.0 too (1.3 for Bond.IO)
    * This adds .NET Standard 1.0 support for all assemblies exception
      Bond.IO.
    * The lowest standard that Bond.IO can target is .NET Standard 1.3, so
      we target that.

--- cs/src/io/unsafe/InputPointer.cs
+++ cs/src/io/unsafe/InputPointer.cs
@@ -233,7 +233,7 @@ namespace Bond.IO.Unsafe
                 EndOfStream(size);
             }
 
-#if (!NETSTANDARD1_6)
+#if !(NETSTANDARD1_3 || NETSTANDARD1_6)
             string result = new string((sbyte*) buffer, position, size, encoding);
 #else
             string result = encoding.GetString(buffer + position, size);
