6dac7e6c32deeb186f15b605d1c82c11c4018f9c
Christopher Warrington <chwarr@microsoft.com>
2016-11-08 16:12:03 -0800

    [c#] Add .NET Core debug, release configurations
    * Build script can be used to pick the configuration.
    * Delay signing--like we use for official Bond releases--can be enabled
      by providing a delay sign key. The official Bond delay sign key is NOT
      part of this repository.

--- cs/dnc/build.ps1
+++ cs/dnc/build.ps1
@@ -1,9 +1,58 @@
-msbuild /m /p:Configuration=Debug /p:Platform=Win32 '..\Compiler.vcxproj'
+<#
+    .SYNOPSIS
+        Builds Bond with the .NET Core toolchain
+
+    .PARAMETER Configuration
+        The configuration to build. Valid values are:
+        - debug
+        - release
+
+    .PARAMETER DelaySignKey
+        The optional delay sign key to use.
+
+    .EXAMPLE
+        PS c:\src\bond> .\build.ps1
+
+        This will build the debug configuration.
+
+    .EXAMPLE
+        PS c:\src\bond> .\build.ps1 -Configuration release -DelaySignKey c:\src\private\delay-key.snk
+
+        This will build the release configuration and delay sign it with the specified key.
+#>
+[CmdletBinding(SupportsShouldProcess=$True)]
+param
+(
+    [ValidateSet("debug", "release")]
+    [string]
+    $Configuration = "debug",
+
+    [string]
+    $DelaySignKey = ""
+)
+
+$script:dotnet_cfg = $Configuration
+
+if ($DelaySignKey) {
+    mkdir -Force 'keys'
+    if (-not $?) {
+        throw "Could not create keys/ directory"
+    }
+
+    Copy-Item -Force $DelaySignKey 'keys/delay-sign.snk'
+    if (-not $?) {
+        throw "Copying delay sign key '$DelaySignKey' to 'keys/delay-sign.snk' failed"
+    }
+
+    $script:dotnet_cfg += "-delay"
+}
+
+msbuild /m /p:Configuration=$Configuration /p:Platform=Win32 '..\Compiler.vcxproj'
 if (-not $?) {
     throw "Building GBC failed."
 }
 
-msbuild /m 'dirs.proj'
+msbuild /m /p:Configuration=$Configuration 'dirs.proj'
 if (-not $?) {
     throw "Code generation failed."
 }
@@ -18,7 +67,7 @@ if (-not $?) {
 # before any of the other projects.
 #
 # See also: https://github.com/dotnet/cli/issues/2639
-dotnet build '.\src\attributes\project.json' '.\src\reflection\project.json' '**\project.json'
+dotnet build --configuration $script:dotnet_cfg '.\src\attributes\project.json' '.\src\reflection\project.json' '**\project.json'
 if (-not $?) {
     throw ".NET Core build failed."
 }
