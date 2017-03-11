4f496b13283d934a6ff0277bff9b148cfb5fb5af
Christopher Warrington <chwarr@microsoft.com>
2017-03-10 20:03:22 -0800

    [c++] Build Bond without _CTR_SECURE_NO_WARNINGS
    When using MSVC, Bond now builds with /sdl enabled and without needing
    _CTR_SECURE_NO_WARNINGS or _SCL_SECURE_NO_WARNINGS. For teams that use
    Bond and follow Microsoft's SDL, this helps them take advantage of some
    MSVC features.

---
+++ cpp/inc/bond/core/detail/sdl.h
@@ -0,0 +1,49 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+/* Microsoft publishes the Security Development Lifecycle (SDL) which "is a
+ * software development process that helps developers build more secure
+ * software and address security compliance requirements while reducing
+ * development cost."
+ *
+ * Some more details at https://www.microsoft.com/en-us/sdl/
+ *
+ * The code in this file allows Bond to access features in MSVC that are
+ * specific to the SDL while maintaining Bond's cross-compiler and
+ * cross-platform compability.
+ */
+
+#pragma once
+
+#include <iterator>
+
+namespace bond { namespace detail
+{
+
+#ifdef _MSC_VER
+
+template<class Iterator> inline
+stdext::checked_array_iterator<Iterator> make_checked_array_iterator(
+    Iterator array,
+    size_t size,
+    size_t index = 0)
+{
+    // Allows algorithms like std::copy to work on pointers by encoding
+    // bounds information.
+    return stdext::checked_array_iterator<Iterator>(array, size, index);
+}
+
+#else
+
+template<class Iterator> inline
+Iterator make_checked_array_iterator(
+    Iterator array,
+    size_t /* size */,
+    size_t /* index */ = 0)
+{
+    return array;
+}
+
+#endif
+
+}}
