df019137e3c93d4ab73a74e7a748b02cf18f9a9d
Simon Gansky <sigansky@microsoft.com>
2017-05-24 17:08:27 -0700

    Java: Update code generator to emit raw integer constants for enum values.

--- compiler/src/Language/Bond/Codegen/Java/SerializationMethods.hs
+++ compiler/src/Language/Bond/Codegen/Java/SerializationMethods.hs
@@ -219,7 +219,7 @@ readValue java fieldType varName depth = case fieldType of
     BT_Set e                 -> readSequence java e varName depth
     BT_Map k v               -> readMap java k v varName depth
     BT_UserDefined Enum {} _ ->
-        [lt|#{varName} = new #{getTypeName java fieldType}(reader.readInt32());|]
+        [lt|#{varName} = #{getTypeName java fieldType}.get(reader.readInt32());|]
     -- FIXME: Recursive types will cause infinite recursion.
     BT_UserDefined _ _       ->
         [lt|#{varName} = new #{getTypeName java fieldType}(); #{varName}.deserialize(reader);|]
