3ba1979a93192b6fd82630399eb1cac303aca22a
Simon Gansky <sigansky@microsoft.com>
2017-05-24 17:08:27 -0700

    Java: Update code generator to emit raw integer constants for enum values.

--- compiler/src/Language/Bond/Codegen/Java/Enum_java.hs
+++ compiler/src/Language/Bond/Codegen/Java/Enum_java.hs
@@ -30,37 +30,73 @@ package #{javaPackage};
 
     typeDefinition Enum {..} = [lt|
 #{Java.generatedClassAnnotations}
-public class #{declName} {
-    #{newlineSep 1 constant enumConstantsWithInt}
+public final class #{declName} {
+
+    public static final class Values {
+        private Values() {}
+
+        #{newlineSep 2 constantIntValueDecl enumConstantsWithInt}
+    }
+
+    #{newlineSep 1 constantObjectDecl enumConstantsWithInt}
 
     public final int value;
 
     public #{declName}(int value) { this.value = value; }
 
     @Override
-    public boolean equals(Object other) {
-        if (!(other instanceof #{declName})) { return false; }
-        return this.value == ((#{declName}) other).value;
-    }
+    public boolean equals(Object other) { return (other instanceof #{declName}) && (this.value == ((#{declName}) other).value); }
+ 
+    @Override
+    public int hashCode() { return this.value; }
 
     @Override
-    public int hashCode() {
-        return this.value;
+    public String toString() { return String.valueOf(this.value); }
+
+    public static #{declName} get(int value) {
+        switch (value) {
+            #{newlineSep 3 switchCaseConstantMapping enumConstantsWithIntDistinct}
+            default: return new #{declName}(value);
+        }
     }
 }|]
       where
-        -- constant
-        constant Constant {..} = let value x = [lt|#{x}|] in
-            [lt|public static final #{declName} #{constantName} = new #{declName}(#{optional value constantValue});|]
+        -- constant object
+        constantObjectDecl c@Constant {..} =
+            [lt|public static final #{declName} #{constantName} = #{enumObjectAssigmentValue c};|]
 
-        -- Process constants to make sure all values can be converted
-        -- to integer.
+        -- constant int
+        constantIntValueDecl Constant {..} = let value x = [lt|#{x}|] in
+            [lt|public static final int #{constantName} = #{optional value constantValue};|]
+ 
+        -- switch cases that map int to object
+        switchCaseConstantMapping Constant {..} =
+            [lt|case Values.#{constantName}: return #{constantName};|]
+
+        -- Process constants to make sure every constant value is set (either explicit or auto-generated)
         enumConstantsWithInt = fixEnumWithInt 0 enumConstants []
-
+ 
         fixEnumWithInt :: Int -> [Constant] -> [Constant] -> [Constant]
         fixEnumWithInt _ [] result = reverse result
         fixEnumWithInt nextInt (h:r) result = case constantValue h of
           Just n -> fixEnumWithInt (n + 1) r (h:result)
           _ -> fixEnumWithInt (nextInt + 1) r ((Constant (constantName h) (Just nextInt)):result)
+
+        -- Process constants to make sure every constant value is distinct
+        enumConstantsWithIntDistinct = findEnumConstantsDistinct enumConstantsWithInt []
+
+        findEnumConstantsDistinct :: [Constant] -> [Maybe Int] -> [Constant]
+        findEnumConstantsDistinct [] _ = []
+        findEnumConstantsDistinct (h:r) keys = if elem (constantValue h) keys
+            then findEnumConstantsDistinct r keys
+            else h : findEnumConstantsDistinct r ((constantValue h):keys)
+
+        -- The RHS value to assign to an enum object
+        enumObjectAssigmentValue :: Constant -> Text
+        enumObjectAssigmentValue enumConstant =
+            let firstDeclaredEnumConstant = head (filter (\c -> (constantValue c) == (constantValue enumConstant)) enumConstantsWithIntDistinct) in
+                if firstDeclaredEnumConstant == enumConstant
+                    then [lt|new #{declName}(Values.#{constantName enumConstant})|]
+                    else[lt|#{constantName firstDeclaredEnumConstant}|]
 
     typeDefinition _ = mempty
