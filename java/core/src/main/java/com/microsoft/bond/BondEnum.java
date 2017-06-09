1c95da78d188b6fe7842f820428a9da1ef13ae71
Simon Gansky <sigansky@live.com>
2017-06-09 13:52:32 -0700

    Java: made generated enum classes implement interfaces and updated toString (#493)

---
+++ java/core/src/main/java/com/microsoft/bond/BondEnum.java
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package com.microsoft.bond;
+
+/**
+ * Denotes a Bond enumeration type.
+ * All generated Bond enum classes implement this interface.
+ */
+public interface BondEnum {
+
+    /**
+     * Returns the underlying integer value.
+     *
+     * @return the integer value
+     */
+    int getValue();
+}
