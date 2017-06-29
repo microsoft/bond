6969b59b740fcf508b9813bf8bff6dfadce18675
Christopher Warrington <chwarr@microsoft.com>
2017-06-29 08:40:22 -0700

    [c#] Stamp version info into Bond assemblies
    Support for embedding assembly and file version information has been
    added to the MSBuild and .NET Core builds.
    
    When building Bond, if the environment variable BOND_CORE_VERSION (or,
    for Bond Comm, BOND_COMM_VERSION) is set to a valid NuGet package
    version string, the generated assemblies will have versions details
    embedded in them as well.
    
    * For developer builds without any version set, the version 0.0.0.1 will
      be used.
    * For pre-release builds (versions of the form x.y.z-someTag), the
      version x.y.z.0 will be used.
    * For release builds (version of the form x.y.z), the version x.y.z.100
      will be used.
    
    Fixes https://github.com/Microsoft/bond/issues/325
    Closes https://github.com/Microsoft/bond/pull/514

--- cs/test/compat/grpc/shared/Properties/AssemblyInfo.cs
+++ cs/test/compat/grpc/shared/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5d90c693-10df-4378-8073-0d8e58d1245f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
