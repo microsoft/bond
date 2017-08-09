c0035f8ad22be2c9d0d11a17736e12478927aa1e
Ted Stein <teds@microsoft.com>
2017-08-09 16:08:31 -0700

    c++ core: Add documentation for untagged bonded.

--- cpp/inc/bond/protocol/simple_binary.h
+++ cpp/inc/bond/protocol/simple_binary.h
@@ -60,6 +60,12 @@
 
                             key, mapped     each item encoded according to its type
 
+                                           .-------. .-----------.
+                    bonded                 | count | | marshaled |
+                                           '-------' '-----------'
+                            count           uint32 count of bytes (always fixed-width, even in v2)
+
+                            marshaled       a marshaled payload
 */
 
 namespace bond
