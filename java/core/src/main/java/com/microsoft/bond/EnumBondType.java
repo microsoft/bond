dc86c01ca03b96b69d58e3cca3138595c87839b9
Ted Stein <teds@microsoft.com>
2017-08-09 14:57:55 -0700

    java core: Fix a typo.

--- java/core/src/main/java/com/microsoft/bond/EnumBondType.java
+++ java/core/src/main/java/com/microsoft/bond/EnumBondType.java
@@ -18,7 +18,7 @@ public abstract class EnumBondType<TEnum extends BondEnum<TEnum>> extends Primit
     private final TEnum defaultValue;
 
     /**
-     * Used by generated subclasses (and only by generated sublclasses) to instantiate the type descriptor.
+     * Used by generated subclasses (and only by generated subclasses) to instantiate the type descriptor.
      */
     protected EnumBondType() {
         // a default for enum type is the enum value with the underlying integer set to 0
