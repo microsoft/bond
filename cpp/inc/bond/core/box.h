a8c2528f94af414f70980d0326b76b58641e2283
Christopher Warrington <chwarr@microsoft.com>
2017-09-27 16:13:29 -0700

    [c++ grpc] Fix missing headers; add scalar example
    * If a service imported bond.bond or bond_const.bond, an #include of
      bond_grpc.h/bond_const_grpc.h was generated, but this header was
      missing. Now, when the CMake variable BOND_ENABLE_GRPC is true, these
      headers are generated.
    * Other minor CMakeLists.txt cleanups.
    * An example, demonstrating how to use bond.Box to wrap scalar service
      arguments has been added. This also serves to test the fixed headers.
    * A helper function bond::make_box has been added to enable creation of
      bond::Box<T> instances via type deduction.

---
+++ cpp/inc/bond/core/box.h
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma once
+
+#include <bond/core/bond_types.h>
+#include <type_traits>
+
+
+namespace bond
+{
+
+template <typename T>
+Box<typename std::remove_reference<T>::type> make_box(T&& arg)
+{
+    Box<typename std::remove_reference<T>::type> b;
+    b.value = std::forward<T>(arg);
+    return b;
+}
+
+} // namespace bond
