872a51654345904ae6bfe7e2606275fea2bbf1d5
Chad Walters <cwalter@microsoft.com>
2017-10-03 11:31:16 -0700

    [C#] Include aliased blob in expressions test

--- cs/test/expressions/Program.cs
+++ cs/test/expressions/Program.cs
@@ -108,6 +108,26 @@
         string IDebugView.DebugView { get { return debugView; } }
     }
 
+    public static class BondTypeAliasConverter
+    {
+        public static decimal Convert(ArraySegment<byte> value, decimal unused)
+        {
+            var bits = new int[value.Count / sizeof(int)];
+            Buffer.BlockCopy(value.Array, value.Offset, bits, 0, bits.Length * sizeof(int));
+
+            return new decimal(bits);
+        }
+
+        public static ArraySegment<byte> Convert(decimal value, ArraySegment<byte> unused)
+        {
+            var bits = decimal.GetBits(value);
+            var data = new byte[bits.Length * sizeof(int)];
+            Buffer.BlockCopy(bits, 0, data, 0, data.Length);
+
+            return new ArraySegment<byte>(data);
+        }
+    }
+
     static class Program
     {
         static void Write(string name, IDebugView codegen)
