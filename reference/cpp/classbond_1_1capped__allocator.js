e12b6f11d87550dc2ecf665f0a8c3cea5e80fb9d
Appveyor <bondlab@microsoft.com>
2017-10-20 13:27:06 +0000

    Update documentation

---
+++ reference/cpp/classbond_1_1capped__allocator.js
@@ -0,0 +1,6 @@
+var classbond_1_1capped__allocator =
+[
+    [ "capped_allocator", "classbond_1_1capped__allocator.html#a2f752a3c86a6b5b2e24f2c79fd47f25c", null ],
+    [ "capped_allocator", "classbond_1_1capped__allocator.html#a21503c48084029afe3f6e961e91c8978", null ],
+    [ "capped_allocator", "classbond_1_1capped__allocator.html#a3318155a5655718c00f8c5314e2127cb", null ]
+];
--- reference/cpp/search/classes_0.js
+++ reference/cpp/search/classes_0.js
@@ -1,5 +1,9 @@
 var searchData=
 [
+  ['allocator_5fholder',['allocator_holder',['../classbond_1_1detail_1_1allocator__holder.html',1,'bond::detail']]],
+  ['allocator_5fholder_3c_20alloc_20_3e',['allocator_holder&lt; Alloc &gt;',['../classbond_1_1detail_1_1allocator__holder.html',1,'bond::detail']]],
+  ['allocator_5freference_5ftype_5fworkaround',['allocator_reference_type_workaround',['../structbond_1_1detail_1_1allocator__reference__type__workaround.html',1,'bond::detail']]],
+  ['allocator_5freference_5ftype_5fworkaround_3c_20alloc_2c_20typename_20boost_3a_3aenable_5fif_3c_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20_20std_3a_3ais_5fvoid_3c_20typename_20alloc_3a_3avalue_5ftype_20_3e_20_3e_3a_3atype_20_3e',['allocator_reference_type_workaround&lt; Alloc, typename boost::enable_if&lt;                                                                                  std::is_void&lt; typename Alloc::value_type &gt; &gt;::type &gt;',['../structbond_1_1detail_1_1allocator__reference__type__workaround_3_01_alloc_00_01typename_01boost_25519443c515ac092c3914bd9f87b022.html',1,'bond::detail']]],
   ['always_5ffalse',['always_false',['../structbond_1_1detail_1_1mpl_1_1always__false.html',1,'bond::detail::mpl']]],
   ['append',['append',['../structbond_1_1detail_1_1mpl_1_1append.html',1,'bond::detail::mpl']]],
   ['append_3c_20std_3a_3aconditional_3c_20c_3c_20t_20_3e_3a_3avalue_2c_20list_3c_20t_20_3e_2c_20list_3c_3e_20_3e_3a_3atype_2c_20filter_5ft_3c_20list_3c_20u_2e_2e_2e_20_3e_2c_20c_20_3e_20_3e',['append&lt; std::conditional&lt; C&lt; T &gt;::value, list&lt; T &gt;, list&lt;&gt; &gt;::type, filter_t&lt; list&lt; U... &gt;, C &gt; &gt;',['../structbond_1_1detail_1_1mpl_1_1append.html',1,'bond::detail::mpl']]]
