12dd01e68cd24e5a321e535611ed6651b05921e3
Christopher Warrington <chwarr@microsoft.com>
2017-10-20 18:58:59 -0700

    [c# grpc] Skip compilation for --structs=false
    * The MSBuild codegen targets now detect --structs=false and skip
      automatic compilation of the non-existent `_types.cs` files.
    * Added a gRPC example that show how to use a shared type assembly
      between a client and service.

---
+++ examples/cs/grpc/shared-types-assembly/server/CalcService.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Examples.SharedTypes
+{
+    using System.Threading.Tasks;
+    using Bond.Grpc;
+    using Grpc.Core;
+
+    public class CalcService : Calc.CalcBase
+    {
+        public override Task<IMessage<Response>> Add(IMessage<Request> request, ServerCallContext context)
+        {
+            var r = request.Payload.Deserialize();
+            var response = new Response
+            {
+                Result = r.Num1 + r.Num2
+            };
+
+            return Task.FromResult(Message.From(response));
+        }
+    }
+}
