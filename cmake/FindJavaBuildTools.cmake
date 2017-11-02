23ae4e9af4e505f2e2003ef9fcc0dbb888203845
Ted Stein <teds@microsoft.com>
2017-11-02 13:57:37 -0700

    java: Allow maven publication and clean up.
    This commit removes the need for the mvn binary and converts all
    Java dependencies within the repo to true dependencies, rather than file
    references. It also generates automatic versions for the core and
    gradle-plugin projects from git tags.

--- cmake/FindJavaBuildTools.cmake
+++ cmake/FindJavaBuildTools.cmake
@@ -1,5 +1,4 @@
 find_program (GRADLE_EXECUTABLE gradle)
-find_program (MAVEN_EXECUTABLE mvn)
 
 if (GRADLE_EXECUTABLE)
     execute_process (
@@ -9,14 +8,4 @@ if (GRADLE_EXECUTABLE)
     message (STATUS "${gradle_version}")
 else()
     message (FATAL_ERROR "gradle was not found. Java code cannot be built.")
-endif()
-
-if (MAVEN_EXECUTABLE)
-    execute_process (
-        COMMAND ${MAVEN_EXECUTABLE} --version
-        OUTPUT_VARIABLE maven_version)
-    message (STATUS "maven found at ${MAVEN_EXECUTABLE}")
-    message (STATUS "${maven_version}")
-else()
-    message (FATAL_ERROR "maven was not found. Java code cannot be built.")
 endif()
