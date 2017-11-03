5c0738b364fa877442c0db9dde44b777aae54e9f
Appveyor <bondlab@microsoft.com>
2017-11-03 01:02:24 +0000

    Update documentation

--- reference/cpp/search/all_a.js
+++ reference/cpp/search/all_a.js
@@ -7,8 +7,8 @@ var searchData=
   ['merge',['Merge',['../classbond_1_1bonded.html#a87a687d614562875e322aed7f915f736',1,'bond::bonded::Merge()'],['../namespacebond.html#a95642a5fa0590898e42a981a12fda6f5',1,'bond::Merge()']]],
   ['metadata',['metadata',['../structbond_1_1reflection_1_1_field_template.html#aef3425067490554d13260a0fbd6277fe',1,'bond::reflection::FieldTemplate::metadata()'],['../structbond_1_1reflection_1_1_method_template.html#a01f4dedcf76af9589d6d2a30cd74d2ad',1,'bond::reflection::MethodTemplate::metadata()'],['../structbond_1_1ext_1_1g_r_p_c_1_1reflection_1_1_method_template.html#aac90a60bd549ddf9687341837434c1a0',1,'bond::ext::gRPC::reflection::MethodTemplate::metadata()']]],
   ['method',['method',['../structbond_1_1reflection_1_1_method_template.html#a838d0659e31a7259e753dc9449e2cabe',1,'bond::reflection::MethodTemplate']]],
-  ['methodtemplate',['MethodTemplate',['../structbond_1_1reflection_1_1_method_template.html',1,'bond::reflection']]],
   ['methodtemplate',['MethodTemplate',['../structbond_1_1ext_1_1g_r_p_c_1_1reflection_1_1_method_template.html',1,'bond::ext::gRPC::reflection']]],
+  ['methodtemplate',['MethodTemplate',['../structbond_1_1reflection_1_1_method_template.html',1,'bond::reflection']]],
   ['modifyingtransform',['ModifyingTransform',['../structbond_1_1_modifying_transform.html',1,'bond']]],
   ['multi_5fthreaded_5fcounter',['multi_threaded_counter',['../classbond_1_1multi__threaded__counter.html',1,'bond']]],
   ['multipleinvocationexception',['MultipleInvocationException',['../classbond_1_1ext_1_1g_r_p_c_1_1_multiple_invocation_exception.html',1,'bond::ext::gRPC']]]
--- reference/cpp/search/classes_8.js
+++ reference/cpp/search/classes_8.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['maybe',['maybe',['../classbond_1_1maybe.html',1,'bond']]],
   ['maybe_3c_20uint32_5ft_20_3e',['maybe&lt; uint32_t &gt;',['../classbond_1_1maybe.html',1,'bond']]],
-  ['methodtemplate',['MethodTemplate',['../structbond_1_1reflection_1_1_method_template.html',1,'bond::reflection']]],
   ['methodtemplate',['MethodTemplate',['../structbond_1_1ext_1_1g_r_p_c_1_1reflection_1_1_method_template.html',1,'bond::ext::gRPC::reflection']]],
+  ['methodtemplate',['MethodTemplate',['../structbond_1_1reflection_1_1_method_template.html',1,'bond::reflection']]],
   ['modifyingtransform',['ModifyingTransform',['../structbond_1_1_modifying_transform.html',1,'bond']]],
   ['multi_5fthreaded_5fcounter',['multi_threaded_counter',['../classbond_1_1multi__threaded__counter.html',1,'bond']]],
   ['multipleinvocationexception',['MultipleInvocationException',['../classbond_1_1ext_1_1g_r_p_c_1_1_multiple_invocation_exception.html',1,'bond::ext::gRPC']]]
