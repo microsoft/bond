edb0e6c19a8c684015ab02d7013f868da6831d1c
Ara Ayvazyan <aivazyan@microsoft.com>
2017-11-03 10:46:03 -0700

    fixup! [travis] Cache warm-up stage

--- tools/ci-scripts/linux/build_travis.sh
+++ tools/ci-scripts/linux/build_travis.sh
@@ -6,8 +6,11 @@ pushd $HOME/docker-images
 wget -N https://bondbinaries.blob.core.windows.net/bondbinaries/docker-images/bond-xenial.tar.gz
 popd
 
+LABEL=`echo $@ | tr " " _`
+mkdir -p $HOME/.ccache/$LABEL
+
 time docker load -i $HOME/docker-images/bond-xenial.tar.gz
-docker run -v $HOME/.ccache:/root/.ccache -v $HOME/.stack:/root/.stack -v `pwd`:/root/bond bond-xenial $HOME "$@"
+docker run -v $HOME/.ccache/$LABEL:/root/.ccache -v $HOME/.stack:/root/.stack -v `pwd`:/root/bond bond-xenial $HOME "$@"
 
 # docker runs as root and may leave files in the following directories that are not readable by the travis user
-sudo chown -R travis:travis $HOME/.ccache $HOME/.stack `pwd`
+sudo chown -R $USER:$USER $HOME/.ccache $HOME/.stack `pwd`
