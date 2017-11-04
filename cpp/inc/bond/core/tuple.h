380b2d9e9680ed39e0c0a436e782c877b649b9c8
Ara Ayvazyan <Aivazyan@microsoft.com>
2017-11-03 17:08:30 -0700

    [c++] Add missing Protocols type arg. in tuple

--- cpp/inc/bond/core/tuple.h
+++ cpp/inc/bond/core/tuple.h
@@ -47,23 +47,23 @@ struct schema<std::tuple<T...>>
 };
 
 
-template <typename ...T>
+template <typename... T>
 const Metadata schema<std::tuple<T...>>::type::metadata
     = schema<std::tuple<T...>>::type::GetMetadata();
 
 
-template <typename Writer, typename ...T>
-inline void Pack(Writer& writer, T&&...args)
+template <typename Protocols = BuiltInProtocols, typename Writer, typename... T>
+inline void Pack(Writer& writer, T&&... args)
 {
-    Serialize(std::forward_as_tuple(args...), writer);
+    Serialize<Protocols>(std::forward_as_tuple(args...), writer);
 }
 
 
-template <typename Reader, typename ...T>
-inline void Unpack(Reader reader, T&...arg)
+template <typename Protocols = BuiltInProtocols, typename Reader, typename... T>
+inline void Unpack(Reader reader, T&... arg)
 {
     auto pack = std::tie(arg...);
-    Deserialize(reader, pack);
+    Deserialize<Protocols>(reader, pack);
 }
 
 } // namepsace bond
