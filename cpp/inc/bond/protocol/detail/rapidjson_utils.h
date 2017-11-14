0ef95a1147e6236c757efbb69f37e0ae169c007f
Ara Ayvazyan <Aivazyan@microsoft.com>
2017-11-14 09:40:00 -0800

    [c++] Reduce included headers for JSON

---
+++ cpp/inc/bond/protocol/detail/rapidjson_utils.h
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma once
+
+#include <string>
+
+
+namespace bond
+{
+namespace detail
+{
+
+std::basic_string<uint16_t> utf_to_utf(const char* begin, const char* end);
+
+bool try_lexical_convert(const char* str, uint16_t& result);
+
+
+} // namespace detail
+
+} // namespace bond
+
+
+#ifdef BOND_HEADER_ONLY
+#include "rapidjson_utils_impl.h"
+#endif
---
+++ cpp/src/bond/protocol/detail/rapidjson_utils.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifdef BOND_HEADER_ONLY
+#error This source file should not be compiled when BOND_HEADER_ONLY is defined
+#endif
+
+#include <bond/protocol/detail/rapidjson_utils_impl.h>
