c758ffa117cb37402ba99a37c3af848518e19dc4
Ted Stein <me@tedstein.net>
2017-11-16 16:00:58 -0800

    [java] Allow maven publication and clean up
    This commit removes the need for the mvn binary and converts all
    Java dependencies within the repo to true dependencies, rather than file
    references. It also generates automatic versions for the core and
    gradle-plugin projects from git tags.

--- cmake/Java.cmake
+++ cmake/Java.cmake
@@ -30,26 +30,3 @@ function (add_gradle_build target)
     add_target_to_folder("${target}")
     add_dependencies(java "${target}")
 endfunction()
-
-function (add_maven_install target)
-    set (oneValueArgs AFTER_BUILD JAR)
-    cmake_parse_arguments (arg "${flagArgs}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-    # The path given to -DpomFile is hard-coded to match the output path of
-    # gradle's maven plugin.
-    add_custom_command (
-        COMMAND
-        ${MAVEN_EXECUTABLE}
-        install:install-file
-        -DpomFile=build/poms/pom-default.xml
-        -Dfile=build/libs/${arg_JAR}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/build/java/${target})
-
-    add_custom_target ("${target}"
-        DEPENDS ${arg_AFTER_BUILD}
-        ${CMAKE_CURRENT_BINARY_DIR}/build/java/${target})
-
-    add_target_to_folder("${target}")
-    add_dependencies(java "${target}")
-endfunction()
--- java/gradle-plugin/README.md
+++ java/gradle-plugin/README.md
@@ -3,7 +3,6 @@ containing this README and repeat as follows:
 
 * write some code
 * `gradle build install`
-* `mvn install:install-file -Dfile=build/libs/bond-gradle-1.0.jar -DpomFile=build/poms/pom-default.xml`
 
 This will install the plugin to your local maven repository, which the other
 Java projects' build.gradles will use in preference to maven central.
