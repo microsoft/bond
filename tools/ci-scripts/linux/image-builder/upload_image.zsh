7b6dcedd7a5dde745ed80c218d6da36d3cb7ba17
Christopher Warrington <chwarr@microsoft.com>
2017-12-07 17:37:49 -0800

    [travis] Automate building of Docker images
    * Use `docker pull` to download a pre-built image from
      bondciimages.azurecr.io instead of `wget` then `docker load`.
        * This speeds up image downloading by about 25%.
    * Update tools/ci-scripts/linux/image-builder/README.md to indicate that
      images are built automatically in a Microsoft VSTS build queue.
    * Delete helper build_image.zsh and upload_image.zsh scripts.

--- tools/ci-scripts/linux/image-builder/upload_image.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-set -eux
-
-# Define AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY in this file.
-source ~/.azure/bondbinaries.sh
-
-az storage blob upload -f bond-xenial.tar.gz -n docker-images/bond-xenial.tar.gz -c bondbinaries
