aeaa28edb06a95a60bba67dd5aaf272e573312be
Appveyor <bondlab@microsoft.com>
2017-12-15 04:17:46 +0000

    Update documentation

--- reference/cpp/navtreeindex2.js
+++ reference/cpp/navtreeindex2.js
@@ -1,4 +1,9 @@
 var NAVTREEINDEX2 =
 {
+"value__or__reference_8h_source.html":[3,0,0,0,1,0,27],
+"visit__any_8h_source.html":[3,0,0,0,1,0,28],
+"wait__callback_8h_source.html":[3,0,0,0,2,1,11],
+"wait_callback_example_8cpp-example.html":[4,0],
+"warning_8h_source.html":[3,0,0,0,1,38],
 "wire__protocol_8h_source.html":[3,0,0,0,0,13]
 };
