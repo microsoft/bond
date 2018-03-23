12d12c0720d972ed39fdf983b592863edc9a5038
Christopher Warrington <chwarr@microsoft.com>
2018-03-23 13:25:01 -0700

    fixup! [c++ travis] Build using gRPC's master

--- tools/ci-scripts/linux/build_cpp-grpc-master.zsh
+++ tools/ci-scripts/linux/build_cpp-grpc-master.zsh
@@ -3,13 +3,12 @@
 set -eux
 
 # Get gRPC's current master
-cd "$BOND_ROOT/thirdparty/grpc"
+pushd "$BOND_ROOT/thirdparty/grpc"
 git fetch origin master
 git checkout origin/master
 git submodule sync --recursive
 git submodule update --recursive
-
-cd "$BUILD_ROOT"
+popd
 
 BOND_CMAKE_FLAGS="$BOND_CMAKE_FLAGS -DBOND_SKIP_GBC_TESTS=TRUE -DBOND_SKIP_CORE_TESTS=TRUE -DgRPC_ZLIB_PROVIDER=package"
 source "$BUILD_SCRIPTS/build_cpp-common.zsh"
