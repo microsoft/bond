c94334232be91decf76b5d362eea584006159dff
Christopher Warrington <chwarr@microsoft.com>
2018-04-19 22:28:08 -0700

    [appveyor] Use Boost from NuGet packages if needed
    The AppVeyor builder images contain some versions of Boost, but not all
    the versions that Bond supports for all the compilers that it supports.
    
    This commit:
    
    * Switches to a minimum Boost of 1.61, the minimum supported version as
      of this commit.
    * Adds a script to easily find the pre-installed Boost library.
    * Adds a script to install a given version of Boost from NuGet.org
      packages.
    * Updates the AppVeyor build to install missing versions of Boost from
      NuGet.org.
    * Refactors the AppVeyor build to base the MSVC compiler off of the
      `BOND_VS_VERSION` or the `APPVEYOR_BUILD_WORKER_IMAGE` variable, which
      should set us up to add MSVC 2017 to the build later.
    
    This is the AppVeyor side of https://github.com/Microsoft/bond/issues/771

---
+++ tools/ci-scripts/windows/ConvertTo-MsvcVersion.ps1
@@ -0,0 +1,72 @@
+﻿<#
+.SYNOPSIS
+
+Convert an AppVeyor build worker image string into a MSVC version number.
+
+.PARAMETER ImageName
+
+The AppVeyor build worker image name.
+
+.PARAMETER Format
+
+The output format. One of 'VcToolsetVer', 'Year', or 'CMakeGeneratorNum'.
+
+VcToolsetVer will produce something like '12.0' or '14.1'.
+Year will produce something like '2013' or '2017'.
+CMakeGeneratorNum will produce something like '12' or '15'.
+
+#>
+[CmdletBinding()]
+param
+(
+    [string]
+    $ImageName,
+
+    [ValidateSet('VcToolsetVer','Year', 'CMakeGeneratorNum')]
+    [string]
+    $Format = 'VcToolsetVer'
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+function ToVcToolsetVer([string]$imageName)
+{
+    switch ($imageName)
+    {
+        'Visual Studio 2013' { '12.0' }
+        'Visual Studio 2015' { '14.0' }
+        'Visual Studio 2017' { '14.1' }
+        default { throw "Unknown image name '$imageName'" }
+    }
+}
+
+function ToYear([string]$vcToolsetVer)
+{
+    switch ($vcToolsetVer)
+    {
+        '12.0' { '2013' }
+        '14.0' { '2015' }
+        '14.1' { '2017' }
+        default { throw "Unknown VcToolsetVer '$vcToolsetVer'" }
+    }
+}
+
+function ToCMakeGeneratorNum([string]$vcToolsetVer)
+{
+    switch ($vcToolsetVer)
+    {
+        '12.0' { '12' }
+        '14.0' { '14' }
+        '14.1' { '15' }
+        default { throw "Unknown VcToolsetVer '$vcToolsetVer'" }
+    }
+}
+
+switch ($Format)
+{
+    'VcToolsetVer' { return ToVcToolsetVer $ImageName }
+    'Year' { return ToYear (ToVcToolsetVer $ImageName) }
+    'CMakeGeneratorNum' { return ToCMakeGeneratorNum (ToVcToolsetVer $ImageName) }
+    default { throw "Unknown Format '$Format'" }
+}
---
+++ tools/ci-scripts/windows/Install-Boost.ps1
@@ -0,0 +1,145 @@
+﻿<#
+.SYNOPSIS
+
+Downloads Boost via NuGet and returns the BOOST_ROOT and BOOST_LIBRARYDIR
+locations.
+
+.PARAMETER Version
+
+The Boost version to install.
+
+.PARAMETER Components
+
+The Boost binary components to install. The Boost headers are always installed.
+
+.PARAMETER VcToolsetVer
+
+Which MSVC toolset version to install libraries for, in Visual Studio toolset
+version number format. (E.g., 12.0, 14.1).
+
+.PARAMETER OutputDirectory
+
+Where to install Boost. If not set, creates a directory in the TEMP folder.
+
+#>
+[CmdletBinding(SupportsShouldProcess=$True)]
+param
+(
+    [string]
+    $Version,
+
+    [ValidateSet('12.0', '14.0', '14.1')]
+    [string]
+    $VcToolsetVer,
+    
+    [string[]]
+    $Components = (
+        'boost_chrono',
+        'boost_date_time',
+        'boost_thread',
+        'boost_system',
+        'boost_unit_test_framework'),
+
+    [string]
+    $OutputDirectory = $null
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+function ConvertVcToolsetVer-ToBoostPackageFormat([string]$vcToolsetVer)
+{
+    return $vcToolsetVer.Replace('.', '')
+}
+
+function Install-NuGetPackage([string]$InstallDir, [string]$PackageId, [string]$PackageVersion)
+{
+    $nugetPrereleaseFlag = @()
+    if ($packageVersion.Contains('-')) {
+        $nugetPrereleaseFlag += 'Prerelease'
+    }
+
+    nuget install $packageId `
+        -OutputDirectory $installDir `
+        -Version $packageVersion `
+        -ExcludeVersion `
+        $nugetPrereleaseFlag
+
+    if (-not $?)
+    {
+        throw "NuGet install of '$packageId' '$packageVersion' failed. Exit code $LastExitCode"
+    }
+}
+
+if (-not $OutputDirectory)
+{
+    $OutputDirectory = [System.IO.Path]::Combine(
+        [System.IO.Path]::GetTempPath(),
+        [System.IO.Path]::GetRandomFileName())
+    Write-Debug "OutputDirectory not set. Generated temp folder name: '$OutputDirectory'"
+}
+
+if (-not (Test-Path -LiteralPath $OutputDirectory -PathType Container))
+{
+    mkdir $OutputDirectory | Out-Null
+}
+
+$workDir = [System.IO.Path]::Combine($OutputDirectory, 'w')
+$destDir = [System.IO.Path]::Combine($OutputDirectory, 'd')
+$lib32Dir = [System.IO.Path]::Combine($destDir, 'lib32')
+$lib64Dir = [System.IO.Path]::Combine($destDir, 'lib64')
+mkdir $workDir | Out-Null
+mkdir $destDir | Out-Null
+mkdir $lib32Dir | Out-Null
+mkdir $lib64Dir | Out-Null
+
+function Install-BoostHeaders
+{
+    Write-Progress -Activity 'Installing Boost' -Status "Installing 'boost' (headers)"
+
+    if ($PSCmdlet.ShouldProcess('boost', 'Install NuGet package'))
+    {
+        Install-NuGetPackage `
+            -PackageId 'boost' `
+            -InstallDir $workDir `
+            -PackageVersion $Version
+
+        Move-Item `
+            -Path ([System.IO.Path]::Combine($workDir, 'boost', 'lib', 'native', 'include', 'boost')) `
+            -Destination $destDir
+    }
+}
+
+function Install-BoostComponent([string]$Component)
+{
+    $packageId = "$Component-vc$(ConvertVcToolsetVer-ToBoostPackageFormat $VcToolsetVer)"
+
+    Write-Progress -Activity 'Installing Boost' -Status "Installing '$packageId'"
+
+    if ($PSCmdlet.ShouldProcess($packageId, 'Install NuGet package'))
+    {
+        Install-NuGetPackage `
+            -PackageId $packageId `
+            -InstallDir $workDir `
+            -PackageVersion $Version
+
+        Move-Item `
+            -Path ([System.IO.Path]::Combine($workDir, $packageId, 'lib', 'native', 'address-model-32', 'lib', '*')) `
+            -Destination $lib32Dir
+
+        Move-Item `
+            -Path ([System.IO.Path]::Combine($workDir, $packageId, 'lib', 'native', 'address-model-64', 'lib', '*')) `
+            -Destination $lib64Dir
+    }
+}
+
+Install-BoostHeaders | Out-Null
+
+$Components | % `
+{
+    Install-BoostComponent -Component $PSItem | Out-Null
+}
+
+Write-Progress -Activity 'Installing Boost' -Completed
+
+return @{'BOOST_ROOT' = $destDir; 'BOOST_LIBRARYDIR'= @{'32' = $lib32Dir; '64' = $lib64Dir}}
