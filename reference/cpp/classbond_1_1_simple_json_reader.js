d45f0f6586270fc11c5cc93a47f9cd59ff8ffa11
Appveyor <bondlab@microsoft.com>
2018-04-23 23:33:44 +0000

    Update documentation

--- reference/cpp/classbond_1_1_simple_json_reader.js
+++ reference/cpp/classbond_1_1_simple_json_reader.js
@@ -1,7 +1,7 @@
 var classbond_1_1_simple_json_reader =
 [
-    [ "SimpleJsonReader", "classbond_1_1_simple_json_reader.html#a73f71de66ff16dea1d5521bbe13cc040", null ],
-    [ "SimpleJsonReader", "classbond_1_1_simple_json_reader.html#add5ffe6e7dbb94349fdc7abbee4b0284", null ],
-    [ "GetBuffer", "classbond_1_1_simple_json_reader.html#a3841ff79af08f21508e598db93aa92eb", null ],
-    [ "GetBuffer", "classbond_1_1_simple_json_reader.html#a28e103143d7dbb6a753a12962b356fa3", null ]
+    [ "SimpleJsonReader", "classbond_1_1_simple_json_reader.html#a91ac61439e6d898841afa2d76fe533f3", null ],
+    [ "SimpleJsonReader", "classbond_1_1_simple_json_reader.html#a1a9124f4cf09e06bbbb77c6790f42366", null ],
+    [ "GetBuffer", "classbond_1_1_simple_json_reader.html#aea09397aaf4a33ee5699ae9f571a565b", null ],
+    [ "GetBuffer", "classbond_1_1_simple_json_reader.html#ae760e96edb944be7236326ec72dc3fa2", null ]
 ];
