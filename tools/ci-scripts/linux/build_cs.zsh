39260c7df809d908d3a082c6128d78b982037af9
Eduardo Salinas <edus@microsoft.com>
2018-05-04 15:56:24 -0700

    [fixup] travis curl

--- tools/ci-scripts/linux/build_cs.zsh
+++ tools/ci-scripts/linux/build_cs.zsh
@@ -2,6 +2,8 @@
 
 set -eux
 
+apt-get install curl
+
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
