407300f8253c01178c95cff6623e78617cb72ac7
Eduardo Salinas <edus@microsoft.com>
2018-05-04 17:03:56 -0700

    [fixup] http

--- tools/ci-scripts/linux/build_cs.zsh
+++ tools/ci-scripts/linux/build_cs.zsh
@@ -6,7 +6,7 @@ apt-get -y install curl
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 
 apt-get update
 apt-get -y install apt-transport-https
