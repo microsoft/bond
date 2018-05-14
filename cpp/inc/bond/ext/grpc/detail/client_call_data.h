f0234fc171b6f21b821e1137a49077716155f2c7
Ara Ayvazyan <Aivazyan@microsoft.com>
2018-05-13 20:34:21 -0700

    fixup! Remove redundant Scheduler typedef

--- cpp/inc/bond/ext/grpc/detail/client_call_data.h
+++ cpp/inc/bond/ext/grpc/detail/client_call_data.h
@@ -7,6 +7,7 @@
 
 #include "io_manager_tag.h"
 #include <bond/core/bonded.h>
+#include <bond/ext/grpc/scheduler.h>
 #include <bond/ext/grpc/unary_call_result.h>
 
 #ifdef _MSC_VER
@@ -34,11 +35,7 @@
 #include <memory>
 
 
-namespace bond { namespace ext { namespace gRPC {
-
-using Scheduler = std::function<void(const std::function<void()>& func)>;
-
-namespace detail {
+namespace bond { namespace ext { namespace gRPC { namespace detail {
 
 /// @brief Implementation class that hold the state associated with
 /// outgoing unary calls.
