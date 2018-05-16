60813d5146e7522c6ecef265a18aa1694e4befa2
Ara Ayvazyan <Aivazyan@microsoft.com>
2018-05-15 20:31:35 -0700

    Revert "fixup! Workaround for GCC bug"
    This reverts commit 94d3e68ba8b9b8d1c5c5e8ecbb54951913e9d28f.

--- cpp/inc/bond/ext/grpc/detail/client.h
+++ cpp/inc/bond/ext/grpc/detail/client.h
@@ -52,20 +52,9 @@ public:
     client& operator=(client&& other) = default;
 
 protected:
-#if !defined(__GNUC__) || (__GNUC__ > 7) || (__GNUC__ == 7 && __GNUC_MINOR__ >= 2)
-    using RpcMethod = grpc::internal::RpcMethod;
-#else
-    // Workaround for a bug in GCC < 7.2: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67054.
-    struct RpcMethod : grpc::internal::RpcMethod
+    grpc::internal::RpcMethod make_method(const char* name) const
     {
-        using grpc::internal::RpcMethod::RpcMethod;
-        RpcMethod();
-    };
-#endif
-
-    RpcMethod make_method(const char* name) const
-    {
-        return RpcMethod{ name, grpc::internal::RpcMethod::NORMAL_RPC, _channel };
+        return grpc::internal::RpcMethod{ name, grpc::internal::RpcMethod::NORMAL_RPC, _channel };
     }
 
     template <typename Response, typename Request>
