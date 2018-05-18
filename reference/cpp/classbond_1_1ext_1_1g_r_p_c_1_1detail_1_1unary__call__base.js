6a4858ed4c557f49fa355c368ac9c7e89191f555
Appveyor <bondlab@microsoft.com>
2018-05-18 20:01:14 +0000

    Update documentation

--- reference/cpp/classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.js
+++ reference/cpp/classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.js
@@ -2,9 +2,9 @@ var classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base =
 [
     [ "context", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#aad6bd002d48877a4557b1e59f9d53c4b", null ],
     [ "context", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#aeecda7fa7777f1225a84fc175f0e21b3", null ],
-    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#a9a4539dd9563c3e73331bdf49b76eff1", null ],
-    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#a96f11499482f1d53ee1b8b36504d2e36", null ],
-    [ "FinishWithError", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#afd6c080a16c1a900f84db5d9e28e3124", null ],
-    [ "request", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#aa7c73e941f6c184b7c2655aefbefdada", null ],
-    [ "request", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#aec26dc35bfe2afa1c70930936392a295", null ]
+    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#a29f3272258dca569beb8c2e7a689cb20", null ],
+    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#a32bbb61b082e295405435397b7be5edb", null ],
+    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#a45817429e3c9fb998a61630c4e922987", null ],
+    [ "request", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#a1a5719abe1414f953ea16a358da38700", null ],
+    [ "request", "classbond_1_1ext_1_1g_r_p_c_1_1detail_1_1unary__call__base.html#a9434279269a134bdce3026a4240d1713", null ]
 ];
--- reference/cpp/classbond_1_1ext_1_1g_r_p_c_1_1unary__call.js
+++ reference/cpp/classbond_1_1ext_1_1g_r_p_c_1_1unary__call.js
@@ -3,10 +3,10 @@ var classbond_1_1ext_1_1g_r_p_c_1_1unary__call =
     [ "unary_call", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#a1333007030f1f3e977411ecf620409c1", null ],
     [ "context", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#aad6bd002d48877a4557b1e59f9d53c4b", null ],
     [ "context", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#aeecda7fa7777f1225a84fc175f0e21b3", null ],
-    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#a9a4539dd9563c3e73331bdf49b76eff1", null ],
-    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#a96f11499482f1d53ee1b8b36504d2e36", null ],
-    [ "FinishWithError", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#afd6c080a16c1a900f84db5d9e28e3124", null ],
-    [ "request", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#aa7c73e941f6c184b7c2655aefbefdada", null ],
-    [ "request", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#aec26dc35bfe2afa1c70930936392a295", null ],
+    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#a29f3272258dca569beb8c2e7a689cb20", null ],
+    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#a32bbb61b082e295405435397b7be5edb", null ],
+    [ "Finish", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#a45817429e3c9fb998a61630c4e922987", null ],
+    [ "request", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#a1a5719abe1414f953ea16a358da38700", null ],
+    [ "request", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#a9434279269a134bdce3026a4240d1713", null ],
     [ "share", "classbond_1_1ext_1_1g_r_p_c_1_1unary__call.html#aeb8cc4dd80466daf09bd0e516ce5349f", null ]
 ];
