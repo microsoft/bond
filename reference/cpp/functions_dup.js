dde021a641b46e47ae26bea6d35274fa4a04edac
Appveyor <bondlab@microsoft.com>
2019-01-10 00:38:19 +0000

    Update documentation

--- reference/cpp/functions_dup.js
+++ reference/cpp/functions_dup.js
@@ -18,5 +18,5 @@ var functions_dup =
     [ "u", "functions_u.html", null ],
     [ "v", "functions_v.html", null ],
     [ "w", "functions_w.html", null ],
-    [ "~", "functions_0x7e.html", null ]
+    [ "~", "functions_~.html", null ]
 ];
