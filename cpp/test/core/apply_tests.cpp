e80dec2c3bfc846621aa7194e23f4fa3c21040f3
Ara Ayvazyan <Aivazyan@microsoft.com>
2021-09-10 11:52:24 -0700

    fixup! Add unit test

--- cpp/test/core/apply_tests.cpp
+++ cpp/test/core/apply_tests.cpp
@@ -1,7 +1,9 @@
 #include "precompiled.h"
 #include "apply_tests.h"
-#include "apply_test_apply.h"   // Note that we don't want to include apply_test_reflection.h so that
-                                // we only see pre-generated Apply overloads.
+#include "apply_test_reflection.h"
+#include "apply_test_apply.h"   // Note that pre-generated Apply overloads will be chosen
+                                // even though apply_test_reflection.h is included.
+#include <sstream>
 
 void Init(unittest::apply::Struct& obj)
 {
@@ -134,6 +136,197 @@ Bonded(uint16_t version = bond::v1)
 }
 
 
+class BuildIDL : public bond::Transform
+{
+public:
+    explicit BuildIDL(std::ostream& out)
+        : _out{ out }
+    {}
+
+    template <typename T>
+    bool Base(const T& value) const
+    {
+        GetTypeName(value);
+        return false;
+    }
+
+    void Begin(const bond::Metadata& metadata) const
+    {
+        _qualifiedName = metadata.qualified_name;
+        _this << "struct " << _qualifiedName << "\n{\n";
+    }
+
+    template <typename T>
+    bool Field(uint16_t id, const bond::Metadata& metadata, const T& value) const
+    {
+        _this << "    " << id << ": " << ToString(metadata.modifier) << " ";
+        _this << GetTypeName(value);
+        _this << " " << metadata.name;
+
+        if (metadata.default_value.nothing)
+        {
+            _this << " = nothing";
+        }
+        else
+        {
+            DefaultValue<typename bond::remove_bonded_value<T>::type>(metadata.default_value);
+        }
+
+        _this << ";\n";
+        return false;
+    }
+
+    template <typename T, typename Reader>
+    void Container(const bond::value<T, Reader>& value, std::size_t size) const
+    {
+        UT_AssertAreEqual(size, 0);
+
+        switch (_container)
+        {
+        case bond::BT_LIST:
+            _this << "list";
+            break;
+        case bond::BT_SET:
+            _this << "set";
+        default:
+            assert(false);
+            break;
+        }
+
+        _this << "<" << GetTypeName(value) << ">";
+    }
+
+    template <typename K, typename T, typename Reader>
+    void Container(const bond::value<K, Reader>& key, const bond::value<T, Reader>& value, std::size_t size) const
+    {
+        UT_AssertAreEqual(size, 0);
+
+        switch (_container)
+        {
+        case bond::BT_MAP:
+            _this << "map";
+            break;
+        default:
+            assert(false);
+            break;
+        }
+
+        _this << "<" << GetTypeName(key) << ", " << GetTypeName(value) << ">";
+    }
+
+    template <typename T>
+    bool UnknownField(std::uint16_t /*id*/, const T& /*value*/) const noexcept
+    {
+        return false;
+    }
+
+    void UnknownEnd() const noexcept
+    {}
+
+    void End() const
+    {
+        _this << "};\n";
+        _this.flush();
+        _out << _this.str();
+    }
+
+private:
+    template <typename T, typename Reader, typename boost::enable_if_c<bond::is_basic_type<T>::value && !std::is_enum<T>::value>::type* = nullptr>
+    std::string GetTypeName(const bond::value<T, Reader>& /*value*/) const
+    {
+        return bond::detail::type<T>::name();
+    }
+
+    template <typename T, typename Reader, typename boost::enable_if<std::is_enum<T> >::type* = nullptr>
+    std::string GetTypeName(const bond::value<T, Reader>& /*value*/) const
+    {
+        return bond::detail::type<typename std::underlying_type<T>::type>::name();
+    }
+
+    template <typename T, typename Reader, typename boost::disable_if<bond::is_basic_type<T> >::type* = nullptr>
+    std::string GetTypeName(const bond::value<T, Reader>& value) const
+    {
+        const auto type = bond::GetTypeId(value);
+        if (type == bond::BT_STRUCT)
+        {
+            BuildIDL that{ _out };
+            Apply(that, value);
+            return that._qualifiedName;
+        }
+        else
+        {
+            _container = type;
+            Apply(*this, value);
+            return {};
+        }
+    }
+
+    template <typename T, typename Reader>
+    std::string GetTypeName(const bond::bonded<T, Reader>& value) const
+    {
+        BuildIDL that{ _out };
+        Apply(that, value);
+        return that._qualifiedName;
+    }
+
+    template <typename T, typename boost::enable_if<std::is_same<T, bool> >::type* = nullptr>
+    void DefaultValue(const bond::Variant& value) const
+    {
+        if (value.uint_value != T{})
+            _this << " = " << std::boolalpha << bool(value.uint_value);
+    }
+
+    template <typename T, typename boost::enable_if_c<std::is_unsigned<T>::value && !std::is_same<T, bool>::value>::type* = nullptr>
+    void DefaultValue(const bond::Variant& value) const
+    {
+        if (value.uint_value != T{})
+            _this << " = " << value.uint_value;
+    }
+
+    template <typename T, typename boost::enable_if<bond::is_signed_int_or_enum<T> >::type* = nullptr>
+    void DefaultValue(const bond::Variant& value) const
+    {
+        if (value.int_value != T{})
+            _this << " = " << value.int_value;
+    }
+
+    template <typename T, typename boost::enable_if<std::is_floating_point<T> >::type* = nullptr>
+    void DefaultValue(const bond::Variant& value) const
+    {
+        if (value.double_value != T{})
+            _this << " = " << value.double_value;
+    }
+
+    template <typename T, typename boost::enable_if<bond::is_string<T> >::type* = nullptr>
+    void DefaultValue(const bond::Variant& value) const
+    {
+        if (value.string_value != T{})
+            _this << " = " << value.string_value;
+    }
+
+    template <typename T, typename boost::enable_if<bond::is_wstring<T> >::type* = nullptr>
+    void DefaultValue(const bond::Variant& value) const
+    {
+        if (value.wstring_value != T{})
+        {
+            std::string str(value.wstring_value.size(), ' ');
+            std::transform(value.wstring_value.begin(), value.wstring_value.end(), str.begin(), [](wchar_t wc) { return char(wc); });
+            _this << " = " << str;
+        }
+    }
+
+    template <typename T, typename boost::disable_if<bond::is_basic_type<T> >::type* = nullptr>
+    void DefaultValue(const bond::Variant& /*value*/) const
+    {}
+
+    std::ostream& _out;
+    mutable std::string _qualifiedName;
+    mutable std::ostringstream _this;
+    mutable bond::BondDataType _container;
+};
+
+
+
 template <typename Reader, typename Writer>
 struct Tests
 {
@@ -153,6 +346,14 @@ struct Tests
         Bonded<Reader, Writer, X>(Reader::version);
 
         bond::RuntimeSchema schema = bond::GetRuntimeSchema<X>();
+
+        std::ostringstream s1;
+        Apply<X>(BuildIDL{ s1 });
+        std::ostringstream s2;
+        Apply(BuildIDL{ s2 }, schema);
+        std::string idl1 = s1.str();
+        std::string idl2 = s2.str();
+        UT_AssertAreEqual(idl1, idl2);
     }
 };
 
