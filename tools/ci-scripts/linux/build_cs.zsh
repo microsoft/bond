8cbee0c8519dc0ba97eab8ab8a925af9bca40107
Jan Dupej <jandupej@microsoft.com>
2024-06-26 11:18:47 +0200

    Fixing cs CI paths.

--- tools/ci-scripts/linux/build_cs.zsh
+++ tools/ci-scripts/linux/build_cs.zsh
@@ -19,8 +19,8 @@ msbuild /p:Configuration=Debug /m $BOND_ROOT/cs/cs.sln
 msbuild /p:Configuration=Fields /m $BOND_ROOT/cs/cs.sln
 
 mono /root/NUnit.ConsoleRunner/tools/nunit3-console.exe -framework=mono -labels=All \
-    $BOND_ROOT/cs/test/core/bin/debug/Properties/net45/Bond.UnitTest.dll \
-    $BOND_ROOT/cs/test/core/bin/debug/Fields/net45/Bond.UnitTest.dll \
-    $BOND_ROOT/cs/test/coreNS10/bin/debug/Properties/net45/Bond.UnitTestCoreNS10.dll \
-    $BOND_ROOT/cs/test/coreNS10/bin/debug/Fields/net45/Bond.UnitTestCoreNS10.dll \
-    $BOND_ROOT/cs/test/internal/bin/debug/net45/Bond.InternalTest.dll
+    $BOND_ROOT/cs/test/core/bin/debug/Properties/net462/Bond.UnitTest.dll \
+    $BOND_ROOT/cs/test/core/bin/debug/Fields/net462/Bond.UnitTest.dll \
+    $BOND_ROOT/cs/test/coreNS10/bin/debug/Properties/net462/Bond.UnitTestCoreNS10.dll \
+    $BOND_ROOT/cs/test/coreNS10/bin/debug/Fields/net462/Bond.UnitTestCoreNS10.dll \
+    $BOND_ROOT/cs/test/internal/bin/debug/net462/Bond.InternalTest.dll
